94275d37752e5e8b41084041f59e978ebc8f5bdc
diversen <dennis.iversen@gmail.com>
2011-05-28 02:03:34 +0200

    rawurlencode redirect correct urls.

--- blocks/right_info.php
+++ blocks/right_info.php
@@ -25,6 +25,10 @@ function block_right_info(){
 <a name="fb_share" type="button_count" href="http://www.facebook.com/sharer.php">Del</a><script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
 EOT;
     $info.=$fb_share ."<hr />";
+    $info.= <<<EOT
+<form action="http://www.facebook.com/addfriend.php"><input type="hidden" name="id" value="178272478878935" /><input type="submit" value="Connect with me on Facebook!" /></form>
+EOT;
+    $info.= "<br />\n";
 
     $info.= <<<EOT
 <b>JMS & Co.</b>
