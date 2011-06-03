1bf4f924a1bd61735259bb8e10cd517c70b0e037
diversen <dennis.iversen@gmail.com>
2011-06-03 11:21:57 +0200

    sql

---
+++ mysql/up/1.30.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `order_items` ADD COLUMN `abstract` varchar(1024) DEFAULT '';
+
+ALTER TABLE `order_items` ADD COLUMN `category` int(10) DEFAULT 0;
+
+CREATE TABLE `order_category` (
+      `id` int(10) NOT NULL AUTO_INCREMENT,
+      `name` varchar(255) NOT NULL,
+      `description` text DEFAULT NULL,
+      `file` mediumblob DEFAULT NULL,
+      `file_thumb` mediumblob DEFAULT NULL,
+      `weight` int(10) DEFAULT NULL,
+      PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
