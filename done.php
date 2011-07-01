766b19a4dcc0d06c48672a79f51c15440dbf6bcf
diversen <dennis.iversen@gmail.com>
2011-07-01 04:02:34 +0200

    updated lang da_DK. clean up.

--- done.php
+++ done.php
@@ -1,4 +1,10 @@
 <?php
 
-echo lang::translate('order_process_confirm_message') . "\n\n";
-echo lang::translate('order_process_greetings') . " $_SERVER[SERVER_NAME]";
+$str = lang::translate('order_process_confirm_message') . "<br /><br />";
+$str.= lang::translate('order_process_greetings') . " $_SERVER[SERVER_NAME]";
+
+echo $str;
+
+//$_SESSION['order_done'] = 1;
+//session::setActionMessage($str);
+//header("Location: /order/cart");
