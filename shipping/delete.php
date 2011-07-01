457c5bd719c8ed81a3373afb77ad33b0cc884f16
dennis <dennis@dennis-laptop.(none)>
2011-07-01 22:44:03 +0200

    shipping edit delete. add weight to module.

--- shipping/delete.php
+++ shipping/delete.php
@@ -4,4 +4,30 @@ if (!session::checkAccessControl('order_allow')){
     return;
 }
 
+$id = URI::$fragments[3];
+$type = URI::$fragments[4];
+
+if (isset($_POST['submit'])) {
+    //orderShipping::sanitize();
+    if (!empty(orderShipping::$errors)) {
+        view_form_errors(orderShipping::$errors);
+    } else {
+
+        $res = orderShipping::delete($id);
+        if ($res) {
+            session::setActionMessage(
+                lang::translate('order_shipping_add_action_message'));
+            header("Location: /order/shipping/$type");
+        } else {
+            // should not happen
+            $str = 'Could not delete!';
+            //view_error($str);
+            error_log($str);
+        }
+    }
+}
+
+$row = orderShipping::getOne($id);
+orderShipping::showDeleteForm();
+//orderShipping::displayAll('private');
 
--- shipping/index.php
+++ shipping/index.php
@@ -3,3 +3,5 @@
 if (!session::checkAccessControl('order_allow')){
     return;
 }
+
+echo lang::translate('order_shipping_help');
