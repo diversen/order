e5abb9712d676b246e48697a845b641fe8691608
dennis <dennis@dennis-laptop.(none)>
2011-07-04 16:38:24 +0200

    add weight calculations.

--- confirm.php
+++ confirm.php
@@ -4,10 +4,6 @@ template::setTitle(lang::translate('order_checkout_html_title'));
 
 $cart = new order();
 order::displayConfirm();
-load_post('order_form');
 
-if (@!empty($_COOKIE['order_items'])){
-    templateView::includeModuleView('order', 'confirm_form');
-}
 
 
