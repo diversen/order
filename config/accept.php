c1690042526fa36646df53aefdb27e02577ed872
diversen <dennis.iversen@gmail.com>
2011-10-01 19:58:22 +0200

    make config work.

--- config/accept.php
+++ config/accept.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "hello world";
-
 $text = get_module_ini('order_form_accept_text');
 $filters = get_module_ini('order_filters');
 echo get_filtered_content($filters, $text);
--- config/index.php
+++ config/index.php
@@ -1,3 +1,3 @@
 <?php
 
-config::displayConfig('order');
+configdb::displayConfig('order');
