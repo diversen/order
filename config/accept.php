1e0ca10270041b34ade4a1d9f3f5658a09c214c4
diversen <dennis.iversen@gmail.com>
2011-10-02 15:12:12 +0200

    filters in configdb settings.

--- config/accept.php
+++ config/accept.php
@@ -1,5 +1,3 @@
 <?php
 
-$text = get_module_ini('order_form_accept_text');
-$filters = get_module_ini('order_filters');
-echo get_filtered_content($filters, $text);
+echo $text = get_module_ini('order_form_accept_text');
