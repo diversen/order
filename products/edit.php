f6472cd575fe9aef5be308fd1b627e1977e9f790
diversen <dennis.iversen@gmail.com>
2011-10-04 18:11:21 +0200

    remove moved files

--- products/edit.php
+++ products/edit.php
@@ -10,17 +10,3 @@ if (!session::checkAccessControl('order_allow')){
 }
 
 order::updateItemControl();
-
-/*
-$use_gallery = get_module_ini('order_use_gallery');
-if (isset($use_gallery)) {
-    include_model('order/gallery');
-    orderGallery::updateGalleryControl();
-}
-
-$use_select = get_module_ini('order_use_gallery');
-if (isset($use_select)) {
-    include_model('order/select');
-    orderSelect::updateSelectControl();
-}*/
-
