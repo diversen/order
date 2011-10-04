66719df176dbfb40aff7ff6cb4e55da768fc1914
diversen <dennis.iversen@gmail.com>
2011-10-05 01:33:02 +0200

    more sql. clean up.

--- mysql/down/1.316.sql
+++ mysql/down/1.316.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `order_item_select` DROP COLUMN `title_human`;
 
 ALTER TABLE `order_item_select` DROP COLUMN `reference`;
+
+DROP TABLE IF EXISTS `order_item_select_option`;
--- mysql/up/1.316.sql
+++ mysql/up/1.316.sql
@@ -1,3 +1,15 @@
 ALTER TABLE `order_item_select` ADD `title_human` varchar(255);
 
 ALTER TABLE `order_item_select` ADD `reference` int(10);
+
+DROP TABLE IF EXISTS `order_item_select_option`;
+
+CREATE TABLE `order_item_select_option` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `select_id` int(10) NOT NULL,
+  `title` varchar (255) DEFAULT '',
+  `value` int (10),
+  FOREIGN KEY (`select_id`) REFERENCES `order_item_select` (id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
