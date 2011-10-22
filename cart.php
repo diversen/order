da6db6417271292b80da62383348bfecee8b87f9
Dennis Iversen <dennis.iversen@gmail.com>
2011-10-22 15:05:24 +0200

    clean up.

--- cart.php
+++ cart.php
@@ -7,10 +7,6 @@ $display = get_module_ini('order_display_main');
 if ( !isset($_GET['cat']) || $_GET['cat'] == 0)  {
     orderCategory::displayCatsFull();
 }  else {
-  
-//$cart = new order ();
-    //$cart->displayCart();
     orderCategory::displayHTMLMenu();
     orderCategory::displayCatItems();
-    
 }
