650fecd213338786197dd9e2f8ad6e9a855396be
Dennis Iversen <dennis.iversen@gmail.com>
2012-01-22 11:32:53 +0100

    clean urls incategory. Refactor

--- cart.php
+++ cart.php
@@ -4,11 +4,16 @@ template::setTitle(lang::translate('order_cart_html_title'));
 
 $display = config::getModuleIni('order_display_main');
 
-if ( !isset($_GET['cat']) || $_GET['cat'] == 0)  {
+
+
+orderCategory::redirect();
+$id = orderCategory::getCatId();
+
+if ( !$id)  {
     orderCategory::displayCatsFull();
 }  else {
     orderCategory::displayHTMLMenu();
-    orderCategory::displayCatItems();
+    orderCategory::displayCatItems($id);
 }
 
 $categories = orderCategory::getAll();
