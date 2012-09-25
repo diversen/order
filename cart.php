d8b44933526164fc58f4a864bd3390a385b8a88c
dennis <dennis@dennis-Rev-1-0.(none)>
2012-09-25 20:43:38 +0200

    minor changes

--- cart.php
+++ cart.php
@@ -12,6 +12,12 @@ $id = orderCategory::getCatId();
 if ( !$id)  {
     orderCategory::displayCatsFull();
 }  else {
+    
+    $category = orderCategory::getCategory($id);
+    $base = "/order/cart/$id";
+    $url = strings::utf8Slug($base, $category['name']);
+    http::permMovedHeader($url);
+    
     orderCategory::displayHTMLMenu();
     orderCategory::displayCatItems($id);
 }
