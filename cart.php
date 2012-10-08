346e3e1dfe6b58180463569c2d1365989cdc6cd5
dennis <dennis@dennis-Rev-1-0.(none)>
2012-10-08 21:02:14 +0200

    minor changes to views

--- cart.php
+++ cart.php
@@ -2,9 +2,7 @@
 
 template::setTitle(lang::translate('order_cart_html_title'));
 
-$display = config::getModuleIni('order_display_main');
-
-
+//$display = config::getModuleIni('order_display_main');
 
 orderCategory::redirect();
 $id = orderCategory::getCatId();
