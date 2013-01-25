2720c6556957d4496c61f093f6cfad2657ef5dcd
dennis <dennis@dennis-Rev-1-0.(none)>
2013-01-25 19:24:41 +0100

    refactor

--- gallery/edit.php
+++ gallery/edit.php
@@ -5,6 +5,6 @@ if (!session::checkAccessControl('order_allow')){
 }
 
 $item_id = URI::getInstance()->fragment(3);
-templateView::includeModuleView('order/products', 'admin_menu', array('id' => $item_id));
+view::includeModuleView('order/products', 'admin_menu', array('id' => $item_id));
 gallery::viewGallery(3, 4, array ('no_redirect' => true));
 //orderGallery::updateGalleryControl(3);
